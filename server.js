9e6e8adedfc01b89a58fdc4af6caa5fe21c0c7e2
nikamanthab <nitinnikamanth@gmail.com>
2018-12-27 20:06:21 +0530

    init commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const port = '192.168.43.233';
+
+var app = express();
+app.get("/",(req,res)=>{
+	res.send("hello");	
+})
+app.use(express.static("/media/nikamanth/New Volume/movies"));
+
+app.listen(3000,port);
